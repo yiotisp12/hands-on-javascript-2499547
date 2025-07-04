d349d0a2bf8902b89c30f32183c53a73fda24afb
Panos Yiotis <98241591+yiotisp12@users.noreply.github.com>
2025-07-04 19:04:27 +0000

    Finished: Fetch data from an API and Get access and documentation for an API (Chapter 3)

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "737d3a62cb51004176f3f084251acd2a",
   units: "metric",
   location: "carpinteria,ca,us",
 };
